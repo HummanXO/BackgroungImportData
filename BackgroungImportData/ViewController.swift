52dd5ad8891a88ead839b8caab4043e8417427e9
HummanXO <rxkolchin@gmail.com>
2025-03-12 12:30:40 +0500

    Initial Commit

---
+++ BackgroungImportData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroungImportData
+//
+//  Created by Alexandr on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
